c204b529f5ce00e5e01a92bce04c52159f14bf05
RuttikHiwase <89509037+RuttikHiwase@users.noreply.github.com>
2024-07-16 15:22:38 +0530

    Add files via upload

---
+++ DomManupulation1.js
@@ -0,0 +1,12 @@
+//Accessing DOM Elements with Javascript
+
+//1.document.getElementById(id);---->for getting single element
+
+//2.document.getElementsByClassName(className);--> for getting all elements of class
+
+//3.document.getElementByTagName(tagname);---->select by tag li,div
+
+//4.document.querySelector(Selector);---->.green--->.class
+
+//5.document.querySelectorAll(selector);
+
